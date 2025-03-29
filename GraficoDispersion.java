4d2f12cf9bc801319e2c19f3c5466edec1a2c5aa
Alan Jimenez <adjr879@gmail.com>
2025-03-28 22:45:44 -0600

    Lab 08 Final

--- GraficoDispersion.java
+++ GraficoDispersion.java
@@ -2,7 +2,6 @@ import java.awt.*;
 import java.awt.geom.*;
 
 public class GraficoDispersion extends GraficoBase {
-
     public GraficoDispersion(String titulo, int[] datos, String[] etiquetas) {
         super(titulo, datos, etiquetas);
     }
@@ -13,36 +12,78 @@ public class GraficoDispersion extends GraficoBase {
 
         // Márgenes y dimensiones
         int margenIzq = 60;
-        int margenDer = 100;
+        int margenDer = 60;
         int margenSup = 60;
         int margenInf = 60;
         int anchoGrafico = 600 - margenIzq - margenDer;
         int altoGrafico = 400 - margenSup - margenInf;
 
         // Dibujar título
-        g2d.setFont(new Font("Arial", Font.BOLD, 16));
-        g2d.drawString(titulo, 300 - 80, 30);
+        g2d.setFont(new Font("Arial", Font.BOLD, 18));
+        g2d.setColor(new Color(34, 40, 49));
+        g2d.drawString(titulo, 300 - g2d.getFontMetrics().stringWidth(titulo)/2, 30);
 
-        // Encontrar el valor máximo
+        // Encontrar el valor máximo y escala
         int maximo = calcularEscala();
+        int numDivisiones = 5;
+        int escala = (maximo + numDivisiones - 1) / numDivisiones * numDivisiones;
 
-        // Dibujar ejes
-        g2d.setColor(Color.BLACK);
+        // Dibujar líneas de referencia y etiquetas del eje Y
+        g2d.setColor(new Color(200, 200, 200));
+        g2d.setStroke(new BasicStroke(1.0f));
+        for (int i = 0; i <= numDivisiones; i++) {
+            int y = 400 - margenInf - (i * altoGrafico / numDivisiones);
+            int valor = (i * escala) / numDivisiones;
+
+            // Línea horizontal
+            g2d.draw(new Line2D.Double(margenIzq, y, 600 - margenDer, y));
+
+            // Etiqueta del eje Y
+            g2d.setColor(Color.BLACK);
+            g2d.setFont(new Font("Arial", Font.PLAIN, 12));
+            g2d.drawString(String.valueOf(valor), margenIzq - 25, y + 5);
+            g2d.setColor(new Color(200, 200, 200));
+        }
+
+        // Dibujar ejes principales
+        g2d.setColor(new Color(57, 62, 70));
         g2d.setStroke(new BasicStroke(2));
         g2d.draw(new Line2D.Double(margenIzq, margenSup, margenIzq, 400 - margenInf));
         g2d.draw(new Line2D.Double(margenIzq, 400 - margenInf, 600 - margenDer, 400 - margenInf));
 
+        // Etiqueta del eje Y
+        g2d.setFont(new Font("Arial", Font.BOLD, 14));
+        g2d.rotate(-Math.PI/2);
+        g2d.drawString("Valores", -250, margenIzq - 40);
+        g2d.rotate(Math.PI/2);
+
+        // Etiqueta del eje X
+        g2d.drawString("Meses", 300 - 25, 380);
+
         // Dibujar puntos y etiquetas
         for (int i = 0; i < datos.length; i++) {
             int x = margenIzq + i * (anchoGrafico / (datos.length - 1));
-            int y = 400 - margenInf - (datos[i] * altoGrafico / maximo);
-            g2d.setColor(COLORES_BARRAS[i % COLORES_BARRAS.length]);
-            g2d.fill(new Ellipse2D.Double(x - 3, y - 3, 6, 6));
+            int y = 400 - margenInf - (datos[i] * altoGrafico / escala);
 
-            // Dibujar etiquetas
-            g2d.setFont(new Font("Arial", Font.PLAIN, 12));
+            // Línea vertical punteada
+            g2d.setColor(new Color(200, 200, 200));
+            g2d.setStroke(new BasicStroke(1.0f, BasicStroke.CAP_BUTT, BasicStroke.JOIN_MITER,
+                                        10.0f, new float[]{2.0f}, 0.0f));
+            g2d.draw(new Line2D.Double(x, 400 - margenInf, x, y));
+
+            // Punto de datos
+            g2d.setColor(COLORES_BARRAS[i % COLORES_BARRAS.length]);
+            g2d.setStroke(new BasicStroke(2));
+            g2d.fill(new Ellipse2D.Double(x - 4, y - 4, 8, 8));
+
+            // Etiquetas del eje X
             g2d.setColor(Color.BLACK);
-            g2d.drawString(etiquetas[i], x + 5, y - 5);
+            g2d.setFont(new Font("Arial", Font.PLAIN, 12));
+            g2d.drawString(etiquetas[i], x - g2d.getFontMetrics().stringWidth(etiquetas[i])/2,
+                          400 - margenInf + 20);
+
+            // Valor del punto
+            g2d.drawString(String.valueOf(datos[i]), x - 10, y - 10);
         }
     }
 }
--- GraficoRadar.java
+++ GraficoRadar.java
@@ -27,44 +27,49 @@ public class GraficoRadar extends GraficoBase {
 
         // Encontrar el valor máximo
         int maximo = calcularEscala();
+        int centroX = margenIzq + anchoGrafico / 2;
+        int centroY = margenSup + altoGrafico / 2;
+
+        // Dibujar círculos concéntricos
+        g2d.setColor(new Color(200, 200, 200, 100));
+        for (int i = 1; i <= 5; i++) {
+            int r = (radio * i) / 5;
+            g2d.draw(new Ellipse2D.Double(centroX - r, centroY - r, r * 2, r * 2));
+        }
 
         // Dibujar ejes
         g2d.setColor(new Color(57, 62, 70));
         g2d.setStroke(new BasicStroke(1.5f));
-        int centroX = margenIzq + anchoGrafico / 2;
-        int centroY = margenSup + altoGrafico / 2;
-
         for (int i = 0; i < datos.length; i++) {
             double angle = 2 * Math.PI * i / datos.length;
             int x = (int) (centroX + radio * Math.cos(angle));
             int y = (int) (centroY + radio * Math.sin(angle));
             g2d.draw(new Line2D.Double(centroX, centroY, x, y));
+            g2d.drawString(etiquetas[i], x + 5, y - 5);
         }
 
-        // Dibujar datos
-        g2d.setColor(new Color(0, 123, 255)); // Azul
-        g2d.setStroke(new BasicStroke(2f));
+        // Dibujar datos con relleno
+        Polygon polygon = new Polygon();
+        g2d.setColor(new Color(62, 226, 187, 159));
         for (int i = 0; i < datos.length; i++) {
             double angle = 2 * Math.PI * i / datos.length;
             int x = (int) (centroX + radio * datos[i] / maximo * Math.cos(angle));
             int y = (int) (centroY + radio * datos[i] / maximo * Math.sin(angle));
-            if (i > 0) {
-                double prevAngle = 2 * Math.PI * (i - 1) / datos.length;
-                int prevX = (int) (centroX + radio * datos[i - 1] / maximo * Math.cos(prevAngle));
-                int prevY = (int) (centroY + radio * datos[i - 1] / maximo * Math.sin(prevAngle));
-                g2d.draw(new Line2D.Double(prevX, prevY, x, y));
-            }
-            g2d.fill(new Ellipse2D.Double(x - 3, y - 3, 6, 6));
-
-            // Dibujar etiquetas
-            g2d.setFont(new Font("Arial", Font.PLAIN, 12));
-            g2d.setColor(Color.BLACK);
-            g2d.drawString(etiquetas[i], x + 5, y - 5);
+            polygon.addPoint(x, y);
         }
+        g2d.fillPolygon(polygon);
 
-        double lastAngle = 2 * Math.PI * (datos.length - 1) / datos.length;
-        int lastX = (int) (centroX + radio * datos[datos.length - 1] / maximo * Math.cos(lastAngle));
-        int lastY = (int) (centroY + radio * datos[datos.length - 1] / maximo * Math.sin(lastAngle));
-        g2d.draw(new Line2D.Double(lastX, lastY, centroX + radio * datos[0] / maximo * Math.cos(0), centroY + radio * datos[0] / maximo * Math.sin(0)));
+        // Dibujar contorno del radar
+        g2d.setColor(new Color(0, 123, 255));
+        g2d.setStroke(new BasicStroke(2f));
+        g2d.drawPolygon(polygon);
+
+        // Dibjar puntos de datos
+        for (int i = 0; i < datos.length; i++) {
+            double angle = 2 * Math.PI * i / datos.length;
+            int x = (int) (centroX + radio * datos[i] / maximo * Math.cos(angle));
+            int y = (int) (centroY + radio * datos[i] / maximo * Math.sin(angle));
+            g2d.fill(new Ellipse2D.Double(x - 3, y - 3, 6, 6));
+        }
     }
 }
